11441c84c80b0958fdfe7e6eacb08326138a40ef
mrpark219 <mrpark219@gmail.com>
2026-05-27 22:07:08 +0900

    ✨ gateway 서비스 추가

---
+++ gateway/src/main/kotlin/me/park/gateway/GatewayApplication.kt
@@ -0,0 +1,11 @@
+package me.park.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<GatewayApplication>(*args)
+}
